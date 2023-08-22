bede304496de1b6e47018b37f50fe91aeb6e64a2
Play0552 <Play055@yandex.ru>
2023-08-22 18:40:17 +0700

    Сделал наброски вьюшек и отображение меню через миксин

--- website/main_app/urls.py
+++ website/main_app/urls.py
@@ -3,5 +3,9 @@ from .views import *
 
 
 urlpatterns = [
-    path('', MainHomePage.as_view(), name='home')
+    path('', MainHomePage.as_view(), name='home'),
+    path('about/', AboutUs.as_view(), name='about'),
+    # path('contact/', ContactFormView.as_view(), name='contact'),
+    # path('login/', LoginIn.as_view(), name='login'),
+    # path('register', RegisterUser.as_view(), name='register')
 ]
--- website/main_app/utils.py
+++ website/main_app/utils.py
@@ -3,9 +3,19 @@ from django.core.cache import cache
 
 from .models import *
 
-# menu = [{}]
+menu = [
+    {'title': 'Домой', 'url_name': 'home'},
+    {'title': 'О нас', 'url_name': 'about'},
+    # {'title': 'Контакты', 'url_name': 'contact'},
+    # {'title': 'Войти', 'url_name': 'login'},
+    # {'title': 'Регистрация', 'url_name': 'register'},
+]
 
 
-# class DataMixin:
-#     def get_context_data(self, **kwargs):
-#         contex = kwargs
+class DataMixin:
+    paginate_by = 4
+    def get_user_context(self, **kwargs):
+        context = kwargs
+        user_menu = menu.copy()
+        context['menu'] = user_menu
+        return context
