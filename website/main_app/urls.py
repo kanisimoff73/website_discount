2a61bdfdf6ecf7a371dcd0f42f5f30f507bfb056
kirill <kanisimoff73@gmail.com>
2023-09-07 13:22:48 +0700

    Добавлен путь к магазинам по слагу.

--- website/main_app/urls.py
+++ website/main_app/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 from .views import *
 
-
 urlpatterns = [
     path('', MainHomePage.as_view(), name='home'),
     path('about/', AboutUs.as_view(), name='about'),
     path('contact/', ContactFormView.as_view(), name='contact'),
-    path('products/<slug:cat_slug>/', ProductsView.as_view(), name='products')
+    path('products/<slug:shop_slug>/', ShopsView.as_view(), name='shops'),
+    path('products/<slug:shop_slug>/<slug:cat_slug>/', ProductsView.as_view(), name='products'),
     # path('login/', LoginIn.as_view(), name='login'),
     # path('register', RegisterUser.as_view(), name='register')
 ]
