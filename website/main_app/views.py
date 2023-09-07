b27166da00600b4cb3374f67e7f3154f34eef48c
kirill <kanisimoff73@gmail.com>
2023-09-07 13:24:38 +0700

    Добавил отображение продуктов при переходе по ссылке магазина и отдельно при переходе по ссылке категории.

--- website/main_app/views.py
+++ website/main_app/views.py
@@ -5,18 +5,50 @@ from .utils import *
 
 class MainHomePage(DataMixin, ListView):
     template_name = "main_app/home.html"
+    context_object_name = "products"
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        main_menu = self.get_main_context(title='Главная страница')
+        return dict(list(context.items()) + list(main_menu.items()))
+
+    def get_queryset(self):
+        return Products.objects.all()
 
 
 class ProductsView(DataMixin, ListView):
     template_name = "main_app/home.html"
     context_object_name = "products"
 
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        main_menu = self.get_main_context(title='Продукты')
+        return dict(list(context.items()) + list(main_menu.items()))
+
     def get_queryset(self):
-        return Products.objects.filter(cat__slug=self.kwargs["cat_slug"])
+        return Products.objects.filter(shop__slug=self.kwargs["shop_slug"], cat__slug=self.kwargs["cat_slug"])
+
+
+class ShopsView(DataMixin, ListView):
+    template_name = "main_app/home.html"
+    context_object_name = "products"
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        main_menu = self.get_main_context(title='Магазины')
+        return dict(list(context.items()) + list(main_menu.items()))
+
+    def get_queryset(self):
+        return Products.objects.filter(shop__slug=self.kwargs["shop_slug"])
 
 
 class AboutUs(DataMixin, ListView):
     template_name = 'main_app/about.html'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        main_menu = self.get_main_context(title='О нас')
+        return dict(list(context.items()) + list(main_menu.items()))
 
 
 class ContactFormView(DataMixin, FormView):
