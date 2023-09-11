d3e95eb7bcd6e1fc3730b3d15aa656078cea497a
kirill <kanisimoff73@gmail.com>
2023-09-11 13:42:27 +0700

    Test.

--- website/main_app/models.py
+++ website/main_app/models.py
@@ -1,4 +1,6 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.urls import reverse
 
 
 class Shops(models.Model):
@@ -6,11 +8,26 @@ class Shops(models.Model):
     name = models.CharField(max_length=20, db_index=True, verbose_name="Магазины")
     slug = models.SlugField(max_length=200, unique=True, db_index=True, verbose_name="URL")
 
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("shops", kwargs={"shop_slug": self.slug})
+
+    class Meta:
+        verbose_name_plural = "Магазины"
+
 
 class Categories(models.Model):
     objects = None
     name = models.CharField(max_length=20, db_index=True, verbose_name="Категория")
     slug = models.SlugField(max_length=200, unique=True, db_index=True, verbose_name="URL")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Категории"
 
 
 class Products(models.Model):
@@ -21,3 +38,30 @@ class Products(models.Model):
     link = models.TextField(verbose_name='Ссылка', null=True)
     cat = models.ForeignKey("Categories", on_delete=models.CASCADE, verbose_name="Категории")
     shop = models.ForeignKey("Shops", on_delete=models.CASCADE, verbose_name="Магазины")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Продукты"
+
+
+class Feedback(models.Model):
+    """
+    Модель обратной связи
+    """
+    subject = models.CharField(max_length=255, verbose_name='Тема письма')
+    email = models.EmailField(max_length=255, verbose_name='Email')
+    content = models.TextField(verbose_name='Содержимое письма')
+    time_create = models.DateTimeField(auto_now_add=True, verbose_name='Дата отправки')
+    ip_address = models.GenericIPAddressField(verbose_name='IP отправителя', blank=True, null=True)
+    user = models.ForeignKey(User, verbose_name='Пользователь', on_delete=models.CASCADE, null=True, blank=True)
+
+    class Meta:
+        verbose_name = 'Обратная связь'
+        verbose_name_plural = 'Обратная связь'
+        ordering = ['-time_create']
+        db_table = 'feedback'
+
+    def __str__(self):
+        return f'Вам письмо от {self.email}'
